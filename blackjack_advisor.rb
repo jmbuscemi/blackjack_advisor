215adbe6a253e1a6189e4c40e4a1b7b7d0d096c7
John M Buscemi <jmbuscemi@gmail.com>
2015-05-10 17:19:31 -0400

    Add option for 2 or 4+ decks.  Hard Mode complete #shipit.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,59 +1,78 @@
 puts "Let's play blackjack!"
+puts "How many decks (1, 2, or 4+)? "
+number_decks = gets.chomp.to_i
+
 #Define Options Hash
 #Hard numbers
-hard = Hash.new
-(5..21).each {|i| hard[i] = {} }
-(5..7).each {|n| (2..11).each {|i| hard[n][i] = "hit"}}
-((2..4).to_a + (7..11).to_a).each {|i| hard[8][i] = "hit"}
-(5..6).each {|i| hard[8][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
-(2..6).each {|i| hard[9][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
-(7..11).each {|i| hard[9][i] = "hit"}
+hard = Hash.new("hit")
+(5..21).each {|i| hard[i] = Hash.new("hit")}
+if number_decks == 1
+  (5..6).each {|i| hard[8][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  (2..6).each {|i| hard[9][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  (2..11).each {|i| hard[11][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+elsif number_decks == 2
+  (2..6).each {|i| hard[9][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  (2..11).each {|i| hard[11][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+elsif number_decks >= 4
+  (3..6).each {|i| hard[9][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  (2..10).each {|i| hard[11][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+end
 (2..9).each {|i| hard[10][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
-(10..11).each {|i| hard[10][i] = "hit"}
-(2..11).each {|i| hard[11][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
-((2..3).to_a + (7..11).to_a).each {|i| hard[12][i] = "hit"}
 (4..6).each {|i| hard[12][i] = "stay"}
 (13..16).each {|n| (2..6).each {|i| hard[n][i] = "stay"}}
-(13..16).each {|n| (7..11).each {|i| hard[n][i] = "hit"}}
 (17..21).each {|n| (2..11).each {|i| hard[n][i] = "stay"}}
 
 #Soft numbers
-soft = Hash.new
-(13..21).each {|i| soft[i] = {} }
-(13..16).each {|n|
-  ((2..3).to_a + (7..11).to_a).each {|i| soft[n][i] = "hit"}
+soft = Hash.new("hit")
+(13..21).each {|i| soft[i] = Hash.new("hit")}
+if number_decks == 1
+  (13..14).each {|n|
+    (4..6).each {|i| soft[n][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  }
+  (2..6).each {|i| soft[17][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  ((2..2).to_a + (7..8).to_a + (11..11).to_a).each {|i| soft[18][i] = "stay"}
+  ((2..5).to_a + (7..11).to_a).each {|i| soft[19][i] = "stay"}
+  (6..6).each {|i| soft[19][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just stay"}
+else
+  (13..14).each {|n|
+    (5..6).each {|i| soft[n][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  }
+  (3..6).each {|i| soft[17][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
+  ((2..2).to_a + (7..8).to_a).each {|i| soft[18][i] = "stay"}
+  (2..11).each {|i| soft[19][i] = "stay"}
+end
+(15..16).each {|n|
   (4..6).each {|i| soft[n][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
 }
-(2..6).each {|i| soft[17][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
-(7..11).each {|i| soft[17][i] = "hit"}
-((2..2).to_a + (7..8).to_a + (11..11).to_a).each {|i| soft[18][i] = "stay"}
 (3..6).each {|i| soft[18][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just stay"}
-(9..10).each {|i| soft[18][i] = "hit"}
-((2..5).to_a + (7..11).to_a).each {|i| soft[19][i] = "stay"}
-(6..6).each {|i| soft[19][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just stay"}
 (20..21).each {|n| (2..11).each {|i| soft[n][i] = "stay"}}
 
 #Pairs
-pair = Hash.new
-(2..11).each {|i| pair[i] = {} }
-(2..7).each {|i| pair[2][i] = "split"}
+pair = Hash.new("split")
+(2..11).each {|i| pair[i] = Hash.new("split")}
+if number_decks == 1
+  (9..11).each {|i| pair[3][i] = "hit"}
+  ((2..3).to_a + (7..11).to_a).each {|i| pair[4][i] = "hit"}
+  (8..11).each {|i| pair[6][i] = "hit"}
+  ((9..9).to_a + (11..11).to_a).each {|i| pair[7][i] = "hit"}
+  (10..10).each {|i| pair[7][i] = "stay"}
+elsif number_decks == 2
+  (8..11).each {|i| pair[3][i] = "hit"}
+  ((2..4).to_a + (7..11).to_a).each {|i| pair[4][i] = "hit"}
+  (8..11).each {|i| pair[6][i] = "hit"}
+  (9..11).each {|i| pair[7][i] = "hit"}
+elsif number_decks >= 4
+  (8..11).each {|i| pair[3][i] = "hit"}
+  ((2..4).to_a + (7..11).to_a).each {|i| pair[4][i] = "hit"}
+  (7..11).each {|i| pair[6][i] = "hit"}
+  (8..11).each {|i| pair[7][i] = "hit"}
+end
+
 (8..11).each {|i| pair[2][i] = "hit"}
-(2..8).each {|i| pair[3][i] = "split"}
-(9..11).each {|i| pair[3][i] = "hit"}
-((2..3).to_a + (7..11).to_a).each {|i| pair[4][i] = "hit"}
-(4..6).each {|i| pair[4][i] = "split"}
 (2..9).each {|i| pair[5][i] = "DOUBLE DOWN!! Unless you don't have the funds...then just hit"}
 (10..11).each {|i| pair[5][i] = "hit"}
-(2..7).each {|i| pair[6][i] = "split"}
-(8..11).each {|i| pair[6][i] = "hit"}
-(2..8).each {|i| pair[7][i] = "split"}
-((9..9).to_a + (11..11).to_a).each {|i| pair[7][i] = "hit"}
-(10..10).each {|i| pair[7][i] = "stay"}
-(2..11).each  {|i| pair[8][i] = "split"}
-((2..6).to_a + (8..9).to_a).each {|i| pair[9][i] = "split"}
 ((7..7).to_a + (10..11).to_a).each {|i| pair[9][i] = "stay"}
 (2..11).each  {|i| pair[10][i] = "stay"}
-(2..11).each  {|i| pair[11][i] = "split"}
 
 def evaluate_card(card)
   low_cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",]
